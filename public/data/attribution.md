421c151ee83fb30a52d641b36fa6ed5287d6b22a
EC2 Default User <ec2-user@ip-172-31-77-255.ec2.internal>
2021-02-17 20:44:41 +0000

    first build

---
+++ public/data/attribution.md
@@ -0,0 +1,3 @@
+Thank you to Ben Morton <ben.morton91@gmail.com> (http://bmorton.co.uk/)
+
+https://github.com/BTMorton/dnd-5e-srd
---
+++ public/js/dnd.js
@@ -0,0 +1,39 @@
+let db = {};
+
+
+$(function(){
+    $.getJSON( `data/db.json`, function( data ) {
+        db = data;
+      
+        for(let key in db){
+            $("select[name=category]").append($("<option>",{text:db[key].description,value:key}));
+        }
+        
+        $("select[name=category]").on("change",function(e,d){
+            let cat = $("select[name=category]").val();
+            parseCat(cat);
+        })
+    });
+})
+
+function parseCat(cat){
+    $("div[name=catalog] div.table").empty();
+    let table = $("<table>");
+    
+    let tr_heading = $("<tr>");
+    for(let i=0; i<db[cat].table.columns.length; i++){
+        tr_heading.append( $("<th>",{text:db[cat].table.columns[i]}) );
+    }
+    table.append(tr_heading);
+    
+    for(let i=0; i<db[cat].table.rows.length; i++){
+        let tr_row = $("<tr>");
+        for(let c=0; c<db[cat].table.columns.length; c++){
+            tr_row.append($("<td>",{text:db[cat].table.rows[i][c]}))
+        }
+        table.append(tr_row);
+    }
+    
+    $("div[name=catalog] div.table").append(table);  
+    
+}
